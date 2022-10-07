8f48ed39e6b772bbaf40ce9e621c8e75479c3c60
Alex Granado <ultimateragers2@gmail.com>
2022-10-07 15:13:58 -0500

    Finished
    Complete

--- count.py
+++ count.py
@@ -1,3 +1,20 @@
 # count.py
 #
 # 
+
+
+strng = input("Input the string you want to count:")
+
+chars = input("Input the characters you want to count:")
+
+def Convert(chars, strng):
+    count = 0
+    for n in strng:
+       if n in chars:
+        count += 1
+    return count
+
+
+
+#print(Convert(chars))
+print("The number of times", chars,"appears in", strng,"is",Convert(chars, strng),".")
